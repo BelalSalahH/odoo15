1eb2696efc30b08c4aab1bb2aca350c9280bef72
Belal Abdullah <88985240+Belal-Abdullah@users.noreply.github.com>
2022-12-03 13:00:16 +0200

    change domain on suspense account

--- om_account_accountant/models/account_move.py
+++ om_account_accountant/models/account_move.py
@@ -2,6 +2,17 @@
 
 from odoo import fields, models, api
 
+class AccountJournal(models.Model):
+    _inherit ='account.journal'
+
+    suspense_account_id = fields.Many2one(
+        comodel_name='account.account', check_company=True, ondelete='restrict', readonly=False, store=True,
+        compute='_compute_suspense_account_id',
+        help="Bank statements transactions will be posted on the suspense account until the final reconciliation "
+             "allowing finding the right account.", string='Suspense Account',
+        domain=lambda self: "[('deprecated', '=', False), ('company_id', '=', company_id), \
+                                 ('user_type_id.type', 'not in', ('receivable', 'payable'))]")
+
 
 class AccountMove(models.Model):
     _inherit = "account.move"
